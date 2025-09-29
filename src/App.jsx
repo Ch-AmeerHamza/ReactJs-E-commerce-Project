a52aed9f349602fe92e46ee2d53608aa17847e03
Ch-AmeerHamza <4meerh4mz4@gmail.com>
2025-09-29 13:18:31 +0500

    Optimized-Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,16 @@
-
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Home from "./Components/Home"
-import Navbar from "./Components/Navbar"
-import Kameez from "./Components/Kameez";
-import Suiting from "./Components/Suiting";
-import Perfumes from "./Components/Perfumes";
-import Cart from "./Components/Cart";
-import Login from "./Components/Login";
+// App.jsx
+import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import TermsAndCondition from "./Components/TermsAndCondition";
-import FounderInfo from "./Components/FounderInfo";
-import AboutUs from "./Components/AboutUs";
-import ContactUs from "./Components/ContactUs";
-
+import { Outlet } from "react-router-dom";
 
 function App() {
-  
   return (
     <>
-<BrowserRouter>
-<Navbar/>
-<Routes>
-<Route path="/" element={<Home/>}/>
-<Route path="/kameez" element={<Kameez/>}/>
-<Route path="/suiting" element={<Suiting/>}/>
-<Route path="/perfumes" element={<Perfumes/>}/>
-<Route path="/cart" element={<Cart/>}/>
-<Route path="/login" element={<Login/>}/>
-<Route path="/term" element={<TermsAndCondition/>}/>
-<Route path="/founder" element={<FounderInfo/>}/>
-<Route path="/aboutus" element={<AboutUs/>}/>
-<Route path="/contact" element={<ContactUs/>}/>
-</Routes>
-<Footer/>
-</BrowserRouter>
-
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './Store.jsx'
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./Store.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./Components/Home";
+import Kameez from "./Components/Kameez";
+import Suiting from "./Components/Suiting";
+import Perfumes from "./Components/Perfumes";
+import Cart from "./Components/Cart";
+import Login from "./Components/Login";
+import TermsAndCondition from "./Components/TermsAndCondition";
+import FounderInfo from "./Components/FounderInfo";
+import AboutUs from "./Components/AboutUs";
+import ContactUs from "./Components/ContactUs";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="kameez" element={<Kameez />} />
+      <Route path="suiting" element={<Suiting />} />
+      <Route path="perfumes" element={<Perfumes />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="login" element={<Login />} />
+      <Route path="term" element={<TermsAndCondition />} />
+      <Route path="founder" element={<FounderInfo />} />
+      <Route path="aboutus" element={<AboutUs />} />
+      <Route path="contact" element={<ContactUs />} />
+    </Route>
+  ),
+  { basename: "/ReactJs-E-commerce-Project" }
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router} />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
